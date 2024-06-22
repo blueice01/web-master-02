d6e6763dc9b8e287459e3e01d170603e1c1ea27c
blueice01 <wkdma1524@gmail.com>
2024-06-22 21:24:45 +0900

    맨 밑부분 수정 01

--- app/page.tsx
+++ app/page.tsx
@@ -252,7 +252,7 @@ export default function Home() {
         <section className="w-full py-12 md:py-24 lg:py-32 lg:pb-32 bg-gray-100 dark:bg-gray-800 flex justify-center" id="contact" style={{ background: "linear-gradient(#1D232C, #000000)" }}>
           <div className="container grid items-center justify-center gap-4 px-4 md:px-6">
             <div className="space-y-3">
-              <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight text-white text-center">연락처</h2>
+              <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight text-white text-center">문의</h2>
               <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400 text-center">
                 답장이 느립니다.
               </p>
